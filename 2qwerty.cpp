d8099b6c5ce45cfa0b8de3384409ffb1c55a2d89
leedongg2 <168533414+leedongg2@users.noreply.github.com>
2025-08-01 10:08:55 +0900

    Update 2qwerty.cpp

--- 2qwerty.cpp
+++ 2qwerty.cpp
@@ -1,46 +1,83 @@
-# myalgorithm_final.py  (Baseline + Local-Search 개선)
-
-import time, random, importlib, util
-
-# baseline_20250719.zip 의 myalgorithm.py 를 baseline_mod 로 import
-import baseline_mod  # 같은 폴더에 'baseline_mod.py' 로 복사해 두세요!
-
-def algorithm(prob_info, timelimit=60):
-    start = time.time()
-
-    # ① Baseline 실행 – 시간의 50%만 사용
-    base_solution = baseline_mod.algorithm(prob_info, timelimit=timelimit * 0.5)
-
-    chk = util.check_feasibility(prob_info, base_solution)
-    if not chk['feasible']:
-        # Baseline 도 infeasible 이면 그대로 반환
-        return base_solution
-
-    best_obj   = chk['obj']
-    solution   = {p: list(base_solution[p]) for p in base_solution}
-    flat_index = [(p, i) for p in solution for i in range(len(solution[p]))]
-
-    # ② Swap + Insert Local-Search
-    ITER = 120
-    while ITER > 0 and time.time() - start < timelimit * 0.90:
-        ITER -= 1
-        if len(flat_index) < 2:
-            break
-
-        # 두 개의 (port, route_idx) 무작위 선택
-        (p1, i1), (p2, i2) = random.sample(flat_index, 2)
-
-        new_solution = {p: list(solution[p]) for p in solution}
-        (route1, d1) = new_solution[p1][i1]
-        (route2, d2) = new_solution[p2][i2]
-
-        # demand ID 교환
-        new_solution[p1][i1] = (route1, d2)
-        new_solution[p2][i2] = (route2, d1)
-
-        chk2 = util.check_feasibility(prob_info, new_solution)
-        if chk2['feasible'] and chk2['obj'] < best_obj:
-            # 개선되면 채택
-            solution, best_obj = new_solution, chk2['obj']
-
-    return solution
+#include <iostream>
+#include <vector>
+using namespace std;
+class contacts{
+public:
+	string name, num, mail;
+};
+class phone{
+private:
+	vector <contacts> contact;
+	int f;
+public:
+	void add(){
+		contacts c;
+		cout<<"name: "<<endl;
+		cin.ignore();
+		getline(cin,c.name);
+		cout<<"phone number: "<<endl;
+		getline(cin,c.num);
+		cout<<"mail: "<<endl;
+		getline(cin,c.mail);
+		cout<<endl;
+		contact.push_back(c);
+	}
+	void allprint(){
+		for(f=0;f<contact.size();f++){
+			cout<<"name: "<<contact[f].name<<" phone number: "<<contact[f].num<<" mail: "<<contact[f].mail<<endl;
+		}
+	}
+	void find(){
+		string finding;
+		cout<<"name"<<endl;
+		cin>>finding;
+		for(f=0;f<contact.size();f++){
+			if(finding==contact[f].name){
+				cout<<"name: "<<contact[f].name<<" phone number: "<<contact[f].num<<" mail: "<<contact[f].mail<<endl;
+				return;
+			}
+			
+		}
+		cout<<"찾는 연락처가 없습니다"<<endl;
+	}
+	void delet(){
+		string what;
+		char ans;
+		cin>>what;
+		for(f=0;f<contact.size();f++){
+			if(what==contact[f].name){
+				cout<<"Do you want to delete this number?(Y/N)";
+				cin>>ans;
+				if(ans=='Y'){
+					contact.erase(contact.begin()+f);
+					cout<<"deleted"<<endl;
+				}
+				else{
+					cout<<"canceled"<<endl;
+					return;
+				}
+			}
+		}
+	}		
+};
+int main(){
+	int a,b=0,c,f;
+	phone Phone;
+	while(true){
+		cout<<"1.add phone number"<<'\n'<<"2.print all phone number"<<'\n'<<"3.search phone number"<<'\n'<<"4.delete phone number"<<'\n'<<"5.modify phone number"<<'\n'<<"6.end this progam"<<endl;
+		cout<<"write number"<<endl;
+		cin>>a;
+		switch(a){
+			case 1:Phone.add(); break;
+			case 2:Phone.allprint(); break;
+			case 3:Phone.find(); break;
+			case 4:Phone.delet(); break;
+			case 5:cout<<"꼬우면 니가 만들던가"; break;
+			case 6:b++; break;
+		}
+		if(b!=0){
+			break;
+		}
+	}
+	
+}
